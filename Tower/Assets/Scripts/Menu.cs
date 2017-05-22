b2e641d7a22ce7ae0229e626534372453178a3ee
Arthur B. de Oliveira <arthurboliveira93@gmail.com>
2017-05-22 20:45:08 -0300

    - Trabalhando na Newgrounds API;

--- Tower/Assets/Scripts/Menu.cs
+++ Tower/Assets/Scripts/Menu.cs
@@ -7,17 +7,53 @@ using UnityEngine.UI;
 public class Menu : MonoBehaviour
 {
     public GameObject particle;
-    public Text highScore;
+    public io.newgrounds.core ngio_core;
+    public int leaderboardId;
 
-    //private Save save;
     private int direction;
 
     private void Awake()
     {
-        //save = new Save();
         StartCoroutine(ChangeDirection());
 
-        //highScore.text = "Highscore: " + save.HighScore();
+        unlockMedal(leaderboardId);
+    }
+
+    private void onGetLeaderscore(io.newgrounds.results.ScoreBoard.getBoards result)
+    {
+        io.newgrounds.SimpleJSONImportableList list = result.scoreboards;
+
+        for(int i = 0; i < list.Count; i++)
+        {
+            Debug.Log(i + " : " + list[i]);
+        }
+    }
+    
+    private void GetLeaderscore()
+    {
+        io.newgrounds.components.ScoreBoard.getBoards scoreboad = new io.newgrounds.components.ScoreBoard.getBoards();
+        //scoreboad.id = leaderboardId;
+        scoreboad.callWith(ngio_core, onGetLeaderscore);
+    }
+
+    // this will get called whenever a medal gets unlocked via unlockMedal()
+    private void onMedalUnlocked(io.newgrounds.results.Medal.unlock result)
+    {
+        io.newgrounds.objects.medal medal = result.medal;
+        Debug.Log("Medal Unlocked: " + medal.name + " (" + medal.value + " points)");
+    }
+
+    // call this method whenever you want to unlock a medal.
+    private void unlockMedal(int medal_id)
+    {
+        // create the component
+        io.newgrounds.components.Medal.unlock medal_unlock = new io.newgrounds.components.Medal.unlock();
+
+        // set required parameters
+        medal_unlock.id = medal_id;
+
+        // call the component on the server, and tell it to fire onMedalUnlocked() when it's done.
+        medal_unlock.callWith(ngio_core, onMedalUnlocked);
     }
 
     public void Play()
